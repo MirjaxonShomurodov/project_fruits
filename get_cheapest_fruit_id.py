2048d50b45e41f81977f150a634ed76a09a816b4
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-08 14:49:54 +0500

    id

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -8,4 +8,21 @@ def get_cheapest_fruit_id(data: str) -> int:
         int: id of the cheapest fruit
     """
     # your code here
-    pass
+    rows=data.split('\n')
+    data=[]
+    for row in rows[:-1]:
+        data.append(row.split(','))
+    
+    first_row =data[1]
+    max_price=float(first_row[1])
+    max_id = 1
+
+    for i in range(1,len(data)):
+        price=float(data[i][1])
+        if max_price > price:
+            max_price=price
+            max_id = i
+    return max_id + 1
+
+f=open('fruits.csv')
+print(get_cheapest_fruit_id(f.read()))
